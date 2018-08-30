afc0ed1a155c76450d6574156158a0dbae53f645
wroclawianka <dorota.zelga@gmail.com>
2018-08-30 21:28:32 +0200

    Inventory DOM structure added

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React from "react";
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
-    return <p>Inventory</p>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm></AddFishForm>
+      </div>
+    );
   }
 }
 
