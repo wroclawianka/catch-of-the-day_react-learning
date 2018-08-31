8e3ec55023885530d586e0a9a0dd366da791696d
wroclawianka <dorota.zelga@gmail.com>
2018-08-31 09:26:49 +0200

    add fish implemented

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,17 @@ import Inventory from "./Inventory"
 import Order from "./Order"
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +22,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"></Header>
         </div>
         <Order></Order>
-        <Inventory></Inventory>
+        <Inventory addFish={this.addFish}></Inventory>
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,12 @@
 import React from "react";
-import AddFishForm from './AddFishForm';
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
-        <AddFishForm></AddFishForm>
+        <AddFishForm addFish={this.props.addFish}/>
       </div>
     );
   }
