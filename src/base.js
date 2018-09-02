866d5df8b57f9cd20e5eedfefa142cec8b5b1501
wroclawianka <dorota.zelga@gmail.com>
2018-09-02 17:52:27 +0200

    base created

---
+++ src/base.js
@@ -0,0 +1,16 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBSibomSH99LrOlhg-0wnLb3K2EVYzBH_I",
+  authDomain: "catch-of-the-day-dorota-zelga.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-dorota-zelga.firebaseio.com"
+});
+
+const base = Rebase.createClass(firebase.database());
+
+// named export
+export { firebaseApp };
+
+// default export
+export default base;
