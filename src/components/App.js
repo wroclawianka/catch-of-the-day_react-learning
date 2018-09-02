bf2350b6cd831b4ec93db87f2c7b1d0706c9aee5
wroclawianka <dorota.zelga@gmail.com>
2018-09-02 14:51:57 +0200

    Order implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Header from "./Header"
-import Inventory from "./Inventory"
-import Order from "./Order"
-import sampleFishes from "../sample-fishes"
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
-  }
+  };
 
   addFish = fish => {
     const fishes = { ...this.state.fishes };
@@ -19,34 +19,37 @@ class App extends React.Component {
 
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
-  }
+  };
 
-  addToOrder = (key) => {
-    const order = { ...this.state.order};
+  addToOrder = key => {
+    const order = { ...this.state.order };
     order[key] = order[key] + 1 || 1;
     this.setState({ order });
-  }
+  };
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"></Header>
+          <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-            <Fish
-              key={key}
-              index={key}
-              details={this.state.fishes[key]}
-              addToOrder={this.addToOrder}
-            />
-          ))}
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order></Order>
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}></Inventory>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,46 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvialable = fish && fish.status === "available";
+    if (isAvialable) {
+      return (
+        <li key={key}>
+          {count} lbs {fish.name}
+          {formatPrice(count * fish.price)}
+        </li>
+      );
+    } else {
+      return (
+        <li key={key}>Sorry, {fish ? fish.name : "fish"} is not longer avialable</li>
+      );
+    }
+  };
+
   render() {
-    return <p>Order</p>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvialable = fish && fish.status === "available";
+      if (isAvialable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
