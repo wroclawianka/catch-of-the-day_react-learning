d6fc022cb3abb569c3b77510595d5c999a47f5cc
wroclawianka <dorota.zelga@gmail.com>
2018-09-02 18:57:41 +0200

    firebase in use

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,25 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
     const fishes = { ...this.state.fishes };
