395ca292df6822d71d4459a4e2d01c9cb650e7e0
wroclawianka <dorota.zelga@gmail.com>
2018-09-04 22:53:39 +0200

    PropTypes added to Order

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,8 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { formatPrice } from "../helpers";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends React.Component {
+  static propTypes = {
+    index: PropTypes.string,
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    addToOrder: PropTypes.func,
+    removeFromOrder: PropTypes.func
+  }
+
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
