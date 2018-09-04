66cdc42626433fec1364b45e2895399452c95029
wroclawianka <dorota.zelga@gmail.com>
2018-09-04 22:53:31 +0200

    PropTypes added to Inventory

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 
 class Inventory extends React.Component {
+  static propTypes = {
+    addFish: PropTypes.func,
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func,
+    loadSampleFishes: PropTypes.func,
+  }
+
   render() {
     return (
       <div className="inventory">
